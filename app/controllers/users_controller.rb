60b1356fb3ba1f05206450dc70b64b430230da2b
Phuc <phuc.t.vo.bkdn@gmail.com>
2015-10-27 20:52:49 +0700

    [Modify] edit convention in users_controller users_helper and users_controller_test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+# UsersController
 class UsersController < ApplicationController
   def new
     @user = User.new
@@ -10,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Timtronhanh"
+      flash[:success] = 'Welcome to the Timtronhanh'
       redirect_to @user
     else
       render 'new'
@@ -21,6 +22,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :phone,
-                                :password_confirmation)
+                                 :password_confirmation)
   end
 end
